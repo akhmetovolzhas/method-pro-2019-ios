c8bfc692efd47f5baebe0795228dd835c85c5139
Olzhas <akhmetov.olzhas@gmail.com>
2019-07-10 18:13:22 +0600

    day 6

---
+++ DesignTemplate/DesignTemplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DesignTemplate
+//
+//  Created by Olzhas Akhmetov on 7/6/19.
+//  Copyright © 2019 Olzhas Akhmetov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
